f04cee1c70137439fd0930751cd3e93240251f8d
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-04-27 21:23:23 +0300

    Get delete request from client and delete from database

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ const run = async () => {
       res.send(result);
     });
 
+    app.delete("/crafts/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("Delete from database", id);
+      const query = { _id: new ObjectId(id) };
+      const result = await craftCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
